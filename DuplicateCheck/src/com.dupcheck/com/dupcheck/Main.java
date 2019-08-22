de344ee7efccc4726066fb71dc7cb3a10aa6cf7e
7eet <raisimranjeetsingh@gmail.com>
2019-08-22 23:27:46 +0530

    removed method forSecondArgument

--- DuplicateCheck/src/com.dupcheck/com/dupcheck/Main.java
+++ DuplicateCheck/src/com.dupcheck/com/dupcheck/Main.java
@@ -2,6 +2,9 @@
 
 
 package com.dupcheck;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import com.dupcheck.list.*;
 import com.dupcheck.strategy.*;
@@ -9,50 +12,90 @@ import java.nio.file.*;
 import java.io.File;
 
 public class Main{
-	private GetList getList = new GetList();
-	private List<File> duplicateFiles = new ArrayList<>(); 
+
+	private static GetList  getList = new GetList();
+	private static List<File>  duplicateFiles = new ArrayList<>();
+	private static FeatureStrategy strategy = null;
 	public static void main(String[] arg){
-		Main main = new Main();
-		 
+
 		if(arg[0] != null){
-			String argument = arg[0];
-			Path p = Paths.get(argument);
-			if(p.toFile().exists()){
-				main.duplicateFiles = main.getList.listOfDuplicateFiles(p);
-				if(arg.length >= 2){
-				main.forSecondArgument(main.duplicateFiles,arg[1]);
-				}else{
-					System.out.format("Report for Duplicate Files%n");
-					main.duplicateFiles.forEach(System.out::println);
+
+			Path path = Paths.get(arg[0]);
+			if(path.toFile().exists()){
+				duplicateFiles = getList.listOfDuplicateFiles(path);
+			}else{
+				System.out.format("Please specify correct directory.%n");
+			}
+
+			if(arg.length >1 ){
+				String secondArgument = arg[1];
+
+
+				switch(secondArgument){
+
+					case "d" :
+
+						strategy = new DeleteFileStrategy();
+						strategy.execute(duplicateFiles);
+						break;
+
+					case "m" :
+						Path movePath = null;
+						try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+							System.out.println("Specify path where to move files: ");
+							String inputPath = reader.readLine();
+							movePath = Paths.get(inputPath);
+						}catch(IOException io){
+							System.out.println("Error in reading input.");
+							//io.printStackTrace();
+						}
+						strategy = new MoveFileStrategy(movePath);
+						strategy.execute(duplicateFiles);
+						break;
+
+					case "csv" :
+						strategy = new CSVFileStrategy();
+						strategy.execute(duplicateFiles);
+						break;
+
+					case "f" :
+
+						strategy = new ReportFileStrategy();
+						strategy.execute(duplicateFiles);
+						break;
+
+					default:
+						System.out.format("To perform operations on duplicate files you have to give one more argument after path." +
+								"%nOperations are as follows:" +
+								"%n1. -d\t-> to delete files" +
+								"%n2. -m\t-> to move files" +
+								"%n3. -csv\t-> to create report in \".csv\" form." +
+								"%n4. -f\t-> to create report in normal text file.%n");
+						break;
 				}
 			}else{
-				System.out.format("Please specify correct directory.%n");			
+
+				if(duplicateFiles.size() == 0)
+					System.out.println("No Duplicate Files.");
+				else{
+					System.out.format("Report for Duplicate Files%n");
+					duplicateFiles.forEach(System.out::println);
+				}
 			}
-		
+
 		}else{
-			System.out.format("Please run this program with two parameter.%nOne for path and another for operation.%n");
+			System.out.format("Please run this program with two arguments." +
+					"%nOne for path and another for operation.%n");
 		}
-		
 	}
-	
-	private void forSecondArgument(List<File> list , String operation){
-		 FeatureStrategy strategy = null;
-		if(operation.equals("d")){
-			strategy = new DeleteFileStrategy();
-			strategy.execute(list);
-		}if(operation.equals("m")){
-			strategy = new MoveFileStrategy();
-			strategy.execute(list);
-		}if(operation.equals("csv")){
-			strategy = new CSVFileStrategy();
-			strategy.execute(list);
-		}if(operation.equals("f")){
-			strategy = new ReportFileStrategy();
-			strategy.execute(list);
-		}else{
-			System.out.format("To perform operations on duplicate files you have to give parameter after path.%nOperations are as follows:%n1. d\t-> to delete files%n2. m\t-> to move files%n3. csv\t-> to create report in \".csv\" form.%n4. f\t-> to create report in normal text file.%n");	
-		}
-		
-	
-	}
+
+	/*private static void retrieveList(String argument){
+			Path p = Paths.get(argument);
+			if(p.toFile().exists()){
+				duplicateFiles = getList.listOfDuplicateFiles(p);
+			}else{
+				System.out.format("Please specify correct directory.%n");
+			}
+	}*/
 }
+
