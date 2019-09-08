4b248a4220180ec295f2b75642469e60fbe7b924
7eet <raisimranjeetsingh@gmail.com>
2019-09-08 12:05:34 +0530

    execute script to run this application

---
+++ DuplicateFileChecker/DuplicateFileChecker-1.0/run.sh
@@ -0,0 +1,17 @@
+
+
+echo "DuplicateFileChecker"
+
+echo "Enter Full Path of Directory: "
+read dirName
+echo "Below are the operations to perform on duplicate files:"
+echo " m -> move duplicate files to another directory
+ d -> delete duplicate files
+ f -> creates report of duplicate files in a normal text file
+ csv -> creates .csv file which you can open in excel"
+
+echo "Enter Operations to perform:(m,d,f,csv)  "
+read performOperation
+
+java --module-path target -m com.dupcheck/com.dupcheck.Main $dirName $performOperation
+
