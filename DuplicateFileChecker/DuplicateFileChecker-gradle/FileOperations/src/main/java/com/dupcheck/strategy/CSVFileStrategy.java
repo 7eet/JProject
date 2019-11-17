9792c169e1e585b3c4473716356bd6fc9fe0dd2b
7eet <raisimranjeetsingh@gmail.com>
2019-11-17 12:35:59 +0530

    added localized date and time

--- DuplicateFileChecker/DuplicateFileChecker-gradle/FileOperations/src/main/java/com/dupcheck/strategy/CSVFileStrategy.java
+++ DuplicateFileChecker/DuplicateFileChecker-gradle/FileOperations/src/main/java/com/dupcheck/strategy/CSVFileStrategy.java
@@ -5,16 +5,12 @@
 // this file create a new file with .csv extension and add duplicate files.
 package com.dupcheck.strategy;
 import java.util.List;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.io.File;
-import java.io.IOException;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFileAttributes;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.LocalDateTime;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
 import java.nio.charset.Charset;
 public class CSVFileStrategy implements FeatureStrategy {
 	
@@ -24,29 +20,27 @@ public class CSVFileStrategy implements FeatureStrategy {
 	
 	private String filePath = System.getProperty("user.dir")+"/../DuplicateFiles.csv";
 
-	@Override
-	public void execute(List<File> list) {
-			try {
+	@Override public void execute(List<File> list){			
+			try{	
 			
-				if (Files.exists(Paths.get(filePath))) {
+				if(Files.exists(Paths.get(filePath))){
 				
 					writer = new BufferedWriter( new OutputStreamWriter(new FileOutputStream(new File(filePath),true),Charset.forName("UTF-8"))); 
 					createReport(writer,list);
 					writer.write("\n");
-				} else {
+				}else{
 					writer = new BufferedWriter( new OutputStreamWriter(new FileOutputStream(new File(filePath)),Charset.forName("UTF-8")));
 					writer.write("ReportDay,ReportTime,Path,Owner,Group,Size (bytes)\n");
 					writer.flush();
 					createReport(writer,list);
 					writer.write("\n");
 				}
-				System.out.println("Created Report");
-			} catch (IOException exception) {
+			}catch(IOException exception){
 				System.out.println("Error occured in creating csv file.");
-			} finally {
-				try {
+			}finally{
+				try{
 				writer.close();
-				} catch (IOException ioex) {
+				}catch(IOException ioex){
 					System.out.println("Error occured in closing object.");
 				}
 			}
@@ -54,21 +48,24 @@ public class CSVFileStrategy implements FeatureStrategy {
 	
 	
 	private void createReport(BufferedWriter bufferedWriter , List<File> list) throws IOException{
-		
-		writer.write(LocalDate.now().toString()+","+LocalTime.now().toString()+"\n");
+	
+		LocalDateTime dateTime = LocalDateTime.now();
+		DateTimeFormatter format = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+		String[] stringDateTime = dateTime.format(format).split(",");			
+		writer.write(stringDateTime[0].trim()+","+stringDateTime[1].trim()+"\n");
 		writer.flush();
 		
 		list.forEach(
 			e -> {
 			
-			if (Files.exists(e.toPath()) && !Files.isDirectory(e.toPath())) {
-				try {
+			if(Files.exists(e.toPath()) && !Files.isDirectory(e.toPath())){
+				try{				
 					attribute = Files.readAttributes(e.toPath(), PosixFileAttributes.class);
 				
 					writer.write(",,"+e.toPath().toAbsolutePath().toString()+","+attribute.owner().getName()+","+attribute.group().getName()+","+e.length());
 					writer.newLine();
 					writer.flush(); 	
-				} catch(IOException ioex) {
+				}catch(IOException ioex){
 					System.out.println("Error occured while writing file.");
 					//ioex.printStackTrace();
 				}
